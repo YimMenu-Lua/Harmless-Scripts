7208edb42ad917daf6ebcdc420eba4e033f3e110
Harmless <88204341+Harmless05@users.noreply.github.com>
2023-07-31 23:34:47 +0300

    Updated version to 1.0.1

--- Scripts/Self/ragdoll-player.lua
+++ Scripts/Self/ragdoll-player.lua
@@ -1,6 +1,6 @@
 --[[
 Name: ["Ragdoll Player"]
-Version: [1.0.0]
+Version: [1.0.1]
 Description: ["Lua script that allows you to ragdoll yourself once or looped, with customizable options. Visible under the 'Self -> Ragdoll Player' tab."]
 Author: ["Harmless"]
 Discord: ["harmless0"]
