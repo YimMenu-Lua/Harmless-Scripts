8b0b47e7d38bd2e59296c7afd0dc46532961b719
Harmless <88204341+Harmless05@users.noreply.github.com>
2023-07-31 23:27:54 +0300

    Fix Ragdoll Loop giving an error
    * Fix Ragdoll Loop giving an error
    Gives "EXCEPTION_ACCESS_VIOLATION" when enabling "Ragdoll Player [Loop]"
    
    * Added missing arrow symbol

--- Scripts/Self/ragdoll-player.lua
+++ Scripts/Self/ragdoll-player.lua
@@ -1,42 +1,45 @@
 --[[
 Name: ["Ragdoll Player"]
 Version: [1.0.0]
-Description: ["Lua script that allows you to ragdoll yourself once or looped, with customizable options. Visible under the 'Self' tab."]
+Description: ["Lua script that allows you to ragdoll yourself once or looped, with customizable options. Visible under the 'Self -> Ragdoll Player' tab."]
 Author: ["Harmless"]
 Discord: ["harmless0"]
 --]]
 
+ragdollTab = gui.get_tab("GUI_TAB_SELF"):add_tab("Ragdoll Player")
+
 function ragdollPlayer()
-	forceFlags = tostring(ForceFlags:get_value())
-	forcex = tostring(Forcex:get_value())
-	forcey = tostring(Forcey:get_value())
-	forcez = tostring(Forcez:get_value())
-    players = PLAYER.PLAYER_PED_ID()
-	PED.SET_PED_TO_RAGDOLL(players, 10000, 3000, 0, true, true, false)
+	local forceFlags = ForceFlags:get_value()
+	local ragdollType = RagdollTypes:get_value()
+	local forcex = Forcex:get_value()
+	local forcey = Forcey:get_value()
+	local forcez = Forcez:get_value()
+    local players = PLAYER.PLAYER_PED_ID()
+	PED.SET_PED_TO_RAGDOLL(players, -1, 0, ragdollType, true, true, false)
 	ENTITY.APPLY_FORCE_TO_ENTITY(players, forceFlags, forcex, forcey, forcez, 0, 0, 0, 0, false, true, true, false, true)
 end
 
-script.register_looped("ragdollPlayerLoop", function()
-	if loop:is_enabled() then
-		ragdollPlayer()
-		loops = tostring(loopSpeed:get_value())
-		forceFlags = tostring(ForceFlags:get_value())
-		forcex = tostring(Forcex:get_value())
-		forcey = tostring(Forcey:get_value())
-		forcez = tostring(Forcez:get_value())
-    	PLAYERS = PLAYER.PLAYER_PED_ID()
-		PED.SET_PED_TO_RAGDOLL(players, 10000, 3000, 0, true, true, false)
-		ENTITY.APPLY_FORCE_TO_ENTITY(players, forceFlags, forcex, forcey, forcez, 0, 0, 0, 0, false, true, true, false, true)
-		script_util:sleep(loops)
-	end
+script.register_looped("ragdollPlayerLoop", function (script)
+    if loop:is_enabled() then
+        local loops = loopSpeed:get_value()
+        local forceFlags = ForceFlags:get_value()
+		local ragdollType = RagdollTypes:get_value()
+        local forcex = Forcex:get_value()
+        local forcey = Forcey:get_value()
+        local forcez = Forcez:get_value()
+        local players = PLAYER.PLAYER_PED_ID()
+        PED.SET_PED_TO_RAGDOLL(players, -1, 0, ragdollType, true, true, false)
+        ENTITY.APPLY_FORCE_TO_ENTITY(players, forceFlags, forcex, forcey, forcez, 0, 0, 0, 0, false, true, true, false, true)
+        script:sleep(loops)
+    end
 end)
-
-ragdollTab = gui.get_tab("GUI_TAB_SELF"):add_tab("Ragdoll Player")
 
 ragdollTab:add_button("Ragdoll Player [Once]", ragdollPlayer)
 ragdollTab:add_separator()
 loop = ragdollTab:add_checkbox("Ragdoll Player [Loop]")
 loopSpeed = ragdollTab:add_input_int("Loop Speed")
+ragdollTab:add_separator()
+RagdollTypes = ragdollTab:add_input_int("Ragdoll Types 0-3")
 ragdollTab:add_separator()
 ForceFlags = ragdollTab:add_input_int("Force Types 1-5")
 Forcex = ragdollTab:add_input_int("Force X")
